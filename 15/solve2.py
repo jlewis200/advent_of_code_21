a83aa86d3108999005489363694fe2eca0d97baa
justin <18606523+jlewis200@users.noreply.github.com>
2021-12-16 08:44:16 -0500

    Remove old solutions.

--- 15/solve2.py
+++ 15/solve2.py
@@ -4,77 +4,77 @@ import numpy as np
 from code import interact
 
 file_name = "input"
-#file_name = "test_weight"
+#file_name = "test_data"
 
-def update(weight, idx, jdx, idx_, jdx_):
+def update(weight, path, complete, prev, idx, jdx, idx_, jdx_):
+    res = False
+
     if idx_ >= 0 and \
        idx_ < weight.shape[0] and \
        jdx_ >= 0 and \
-       jdx_ < weight.shape[1]:
+       jdx_ < weight.shape[1] and \
+       not complete[idx_, jdx_]:
 
-        tent_path = weight[idx, jdx].path + weight[idx_, jdx_].weight
-        if tent_path < weight[idx_, jdx_].path:
-            weight[idx_, jdx_].path = tent_path
+        tent_path = path[idx, jdx] + weight[idx_, jdx_]
+        if tent_path < path[idx_, jdx_]:
+            path[idx_, jdx_] = tent_path
+            prev[idx_, jdx_] = (idx, jdx)
+            res = True
+
+    return res
  
 def sort_f(node):
-    return node.path
-
-def get_val(val):
-    if val < 10:
-        return val
-    else:
-        return (val % 10) + 1
+    return node[0]
 
 with open(file_name, "r") as in_file:
     weight = list()
-
     jdx = 0
     for line in in_file:
-        numbers = list(line.strip())
-        tmp = [Node(idx, jdx, int(numbers[idx])) for idx in range(len(numbers))]
-        tmp2 = tmp.copy()
-        for idx in range(1, 5):
-            for node in tmp2:
-                tmp += [Node(node.x + (idx * len(tmp2)), node.y, get_val(node.weight + idx))]
-        weight.append(tmp)
+        weight.append(list(line.strip()))
         jdx += 1
 
-    tmp = weight.copy()
+    weight = np.asarray(weight, dtype=np.int64)
+    tile_shape = weight.shape
+    weight = np.tile(weight, (5, 5))
+
+    #increment tiles along x axis
     for idx in range(1, 5):
-        for line in tmp:
-            #tmp2 = [Node(node.x, node.y + (idx * len(tmp)), max(1, (node.weight + idx) % 10)) for node in line]
-            tmp2 = [Node(node.x, node.y + (idx * len(tmp)), get_val(node.weight + idx)) for node in line]
-            weight.append(tmp2)
+        weight[idx * tile_shape[0]:, 0:] += 1
 
-    weight = np.asarray(weight)
-    interact(local=locals())
+    #increment tiles along y axis
+    for idx in range(1, 5):
+        weight[0:, idx * tile_shape[1]:] += 1
+
+    #wrap around if greater than 9
+    weight[weight > 9] -= 9
+
+    complete = np.full(shape=weight.shape, fill_value=False)
+    prev = np.zeros(shape=(weight.shape[0], weight.shape[1], 2), dtype=np.int64)
     path = np.full(shape=weight.shape, fill_value=np.inf)
-    weight[0, 0].path = 0
+    path[0, 0] = 0
 
-    nodes = list()
-    for idx in range(weight.shape[0]):
-        for jdx in range(weight.shape[1]):
-            nodes.append(weight[idx, jdx])
-    
-    while len(nodes) > 0:
-        print("remaining: %d" % len(nodes))
+    nodes = [(path[0, 0], (0, 0))] 
+
+    while not complete[-1, -1] and len(nodes) > 0:
+#        print(len(nodes))
         nodes.sort(key=sort_f)
-        node = nodes.pop(0)
-        idx = node.y
-        jdx = node.x
+        coord = nodes.pop(0)[1]
 
-        #update left
-        update(weight, idx, jdx, idx - 1, jdx)
+        coords_ = [(coord[0] - 1, coord[1]), 
+                   (coord[0] + 1, coord[1]), 
+                   (coord[0], coord[1] - 1), 
+                   (coord[0], coord[1] + 1)]
 
-        #update down
-        update(weight, idx, jdx, idx, jdx + 1)
+        for coord_ in coords_: 
+            if update(weight, path, complete, prev, *coord, *coord_):
+                nodes.append((path[coord_], coord_))
+        
+        complete[coord] = True
 
-        #update right
-        update(weight, idx, jdx, idx + 1, jdx)
+    coord = (-1, -1)
+    shortest_path = [(coord)]
+    while coord != (0, 0):
+        coord = tuple(prev[coord])
+        shortest_path.insert(0, coord)
 
-        #update up
-        update(weight, idx, jdx, idx, jdx - 1)
-            
-
-    print(weight[-1 ,-1].path)
-    interact(local=locals())
+    print(path[-1 ,-1])
